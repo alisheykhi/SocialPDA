fe3c95807d7c52bbc081b8827f374bc7fa83b803
Ali Sheykhi <ali.sheykhi@me.com>
2016-01-21 01:52:09 +0330

    Reading data set and converting to snap.TNUGraph. Data set : PolBooks, PolBlogs, Caida, and Amazon.

--- Degree_Anonimity.py
+++ Degree_Anonimity.py
@@ -5,8 +5,8 @@ import snap
 from graph_util import ReadGraph
 
 graph_blogs = ReadGraph("polblogs.gml")
-#graph_books = ReadGraph("polbooks.gml")
-#graph_caida = ReadGraph("caida.txt")
-#graph_amazoon = ReadGraph("amazon.txt")
+graph_books = ReadGraph("polbooks.gml")
+graph_caida = ReadGraph("caida.txt")
+graph_amazoon = ReadGraph("amazon.txt")
 
 
