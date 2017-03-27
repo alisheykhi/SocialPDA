5ef71d82c22a93ebd0126a00224c9cc8eea33d25
Ali Sheykhi <ali.sheykhi@me.com>
2017-03-27 12:11:15 +0430

    issue Particle PDA still forgot global best. the issue still persists.

--- particle_pda.py
+++ particle_pda.py
@@ -15,6 +15,8 @@ class ParticlePDA():
     iterations = 20
     swarmSize = 100
     solution = []
+    vmax =6
+    globalBest = []
 
     def __init__(self,omega_clusters,beta):
         print "____________________ParticlePDA____________________________"
@@ -68,46 +70,61 @@ class ParticlePDA():
         # print ParticlePDA.differences
 
         particleSwarmOptimizer = ParticleSwarmOptimizer()
-        #particleSwarmOptimizer.optimize()
-        particle = np.random.randint(2, size = len(ParticlePDA.omega_cluster))
-        sum = particleSwarmOptimizer.f(particle)
-        print particle
-        print sum
-        #print particleSwarmOptimizer.solution
+        particleSwarmOptimizer.initParticle()
+        sol = particleSwarmOptimizer.optimize()
+
+        # particle = np.random.randint(2, size = len(ParticlePDA.omega_cluster))
+        # sum = particleSwarmOptimizer.f(particle)
+        # print particle
+        # print sum
+
+        print sol
 
 class ParticleSwarmOptimizer:
     solution = []
     swarm = []
-    globalBest = []
 
     def __init__(self):
-        for h in range(ParticlePDA.swarmSize):
-            self.swarm.append(Particle(ParticlePDA.differences))
         return
 
+    def initParticle(self):
+        for h in range(ParticlePDA.swarmSize):
+            self.swarm.append(Particle())
+            self.swarm[h].pos = np.random.randint(2, size = ParticlePDA.dimension)
+            self.swarm[h].pBest = self.swarm[h].pos
+            self.swarm[h].velocity = np.random.ranf(size=ParticlePDA.dimension)
+        ParticlePDA.globalBest = np.random.randint(1, size = ParticlePDA.dimension)
+
     def optimize(self):
-        self.globalBest = self.swarm[0].pos
-        for i in range(ParticlePDA.iterations):
-            gBest = self.globalBest
-            print "iteration ", i+1 ,"="
+        print ParticlePDA.globalBest
+        self.solution = ParticlePDA.globalBest
+        for i in range(ParticlePDA.iterations): # 0 -> iter-1
+            gbest = self.solution
+            print gbest , self.f(gbest)
+            print "iteration ", i+1 ,"---------------------------"
             for j in range(ParticlePDA.swarmSize):
-                gBest = self.globalBest
                 pBest = self.swarm[j].pBest
-                if self.f(pBest) < self.f(gBest):
-                    gBest = pBest
-                    self.globalBest = gBest
-                    print self.f(gBest)
-            self.solution = gBest
+                if self.f(pBest) < self.f(gbest):
+                    print "first Global",self.f(gbest)
+                    gbest = pBest
+                    print "second one",self.f(gbest)
+                    print gbest
+            self.solution = gbest
+
             #Update position of each paricle
             for k in range(ParticlePDA.swarmSize):
-                self.swarm[k].updateVelocities(gBest)
+                self.swarm[k].updateVelocities(self.solution)
                 self.swarm[k].updatePositions()
+                print self.solution,"sol"
+
+
                 #self.swarm[k].satisfyConstraints()
             #Update the personal best positions
+
             for l in range(ParticlePDA.swarmSize):
-                pBest = self.swarm[l].pBest
-                if self.f(self.swarm[l].pos) < self.f(pBest):
+                if self.f(self.swarm[l].pos) < self.f(self.swarm[l].pBest):
                     self.swarm[l].pBest = self.swarm[l].pos
+
         return self.solution
 
     def f(self, particle):
@@ -143,41 +160,34 @@ class ParticleSwarmOptimizer:
     # This class contains the particle swarm optimization algorithm
 
 class Particle:
-
-
-    def __init__(self,deff):
-        self.velocity = []
-        self.pos = []
-        self.pBest = []
-        for i in range(0,ParticlePDA.dimension):
-            '''
-            if (math.fabs(deff[i][0]) < math.fabs (deff[i][1])):
-                self.pos.append(0)
-            if (math.fabs(deff[i][0]) > math.fabs (deff[i][1])):
-                self.pos.append(1)
-            if (math.fabs(deff[i][0]) == math.fabs (deff[i][1])):
-                self.pos.append(random.randrange(0,2))
-            '''
-            self.pos.append(random.randrange(0,2))
-            self.velocity.append(0.01 * random.random())
-            self.pBest.append(self.pos[i])
-        return
-
+    pos = []
+    velocity =[]
+    pBest = []
+    def __init__(self):
+        # self.pos = np.random.randint(2, size = ParticlePDA.dimension)
+        # self.velocity = np.random.ranf(size=ParticlePDA.dimension)
+        pass
     def updatePositions(self):
         for i in range(ParticlePDA.dimension):
-            if (random.random() >= self.sigmoid(self.velocity[i]) ):
+            if np.random.rand(1) < self.sigmoid(self.velocity[i]):
                 self.pos[i] = 0
             else:
                 self.pos[i] = 1
         return
 
     def updateVelocities(self, gBest):
+
         for i in range(ParticlePDA.dimension):
             r1 = random.random()
             r2 = random.random()
-            social = ParticlePDA.c1 * r1 * (gBest[i] - self.pos[i])
-            cognitive = ParticlePDA.c2 * r2 * (self.pBest[i] - self.pos[i])
-            self.velocity[i] =  social + cognitive #+ (ParticlePDA.w * self.velocity[i])
+            social = float(ParticlePDA.c1 * r1 * (gBest[i] - self.pos[i]))
+            cognitive = float(ParticlePDA.c2 * r2 * (self.pBest[i] - self.pos[i]))
+            velocity = social + cognitive
+            if abs(velocity) > ParticlePDA.vmax and abs(velocity) is velocity:
+                velocity = ParticlePDA.vmax
+            elif abs(velocity) > ParticlePDA.vmax:
+                velocity = -ParticlePDA.vmax
+            self.velocity[i] =  velocity #+ (ParticlePDA.w * self.velocity[i])
         return
 
     def sigmoid (self, x):
