42e0aad5092fc4076a48422377fb35f15b1ca472
Ali Sheykhi <ali.sheykhi@me.com>
2017-08-09 14:42:00 +0430

    Pbest and Gbest

--- swarm_pda.py
+++ swarm_pda.py
@@ -48,16 +48,18 @@ class SwarmPDA():
         #print((list(self.modified_graph.edges())))
         diff = len(self.rho_minus) - len(self.rho_plus)
         print( 'diff : %d' %diff)
-        if diff % 2 != 0:
+        if abs(diff) % 2 != 0:
             if diff >0:
-                diff-=1
-                self.rho_minus.pop(random.randrange(len(self.rho_minus)))
+                diff -=1
+                print self.rho_minus.pop(random.randrange(len(self.rho_minus)))
             if diff <0:
                 diff +=1
-                self.rho_plus.pop(random.randrange(len(self.rho_plus)))
-        if diff > 0:
+                print self.rho_plus.pop(random.randrange(len(self.rho_plus)))
+        diff = len(self.rho_minus) - len(self.rho_plus)
+        print( 'diff : %d' %diff)
+        if diff >= 0:
             print( "we should first delete 1/2 abs(delta(rho))) = %d edges in G:" % math.floor (abs(diff/2.0)))
-            while diff > 0:
+            while len(self.rho_plus) <> len(self.rho_minus):
                 bound = 0
                 while True:
                     bound+=1
@@ -70,16 +72,16 @@ class SwarmPDA():
                             self.rho_minus.insert(r1,s1)
                             self.rho_minus.insert(r2,s2)
                         else:
+                            print 'remove edge %d,%d'%(s1,s2)
                             # print "remove edge %d,%d" %(s1,s2)
                             break
                     if bound >50:
                         logging.error('failed to remove edge %d,%d'%(s1,s2))
                         break
-                diff -= 2
 
         if diff < 0:
             print( "we should first add 1/2 abs(delta(rho))) = %d edges in G:" % math.floor(abs(diff/2.0)))
-            while diff < 0:
+            while len(self.rho_plus) <> len(self.rho_minus):
                 bound = 0
                 while True:
                     bound+=1
@@ -91,70 +93,16 @@ class SwarmPDA():
                         if not self.edge_add(s1,s2):
                             self.rho_plus.insert(r1,s1)
                             self.rho_plus.insert(r2,s2)
+
                         else:
-                            # print "add edge %d,%d" %(s1,s2)
+                            print 'add edge %d,%d'%(s1,s2)
                             break
                     if bound >50:
                         logging.error('failed to add edge %d,%d'%(s1,s2))
                         break
-                diff += 2
-
-
-        print 'final Rho diff :', len(self.rho_minus)-len(self.rho_plus)
-        # add_rmv= 0
-        #
-        # for i in range(0,len(DecDegNode),2):
-        #     bound = 0
-        #     while True:
-        #         bound+=1
-        #         s1 = random.choice(DecDegNode)
-        #         s2 = random.choice(DecDegNode)
-        #         if s1 != s2:
-        #             if self.edge_removal(s1,s2):
-        #                 DecDegNode.remove(s1)
-        #                 DecDegNode.remove(s2)
-        #                 add_rmv += 1
-        #                 #print( 'remove edge (%d,%d)'%(s1,s2))
-        #                 break
-        #             else:
-        #                 self.logger.warning('failed to remove edge %d,%d'%(s1,s2))
-        #         if bound >50:
-        #             break
-        #
-        # for i in range(0,len(IncDegNode),2):
-        #     bound =0
-        #     while True:
-        #         bound +=1
-        #         s1 = random.choice(IncDegNode)
-        #         s2 = random.choice(IncDegNode)
-        #         if s1 != s2:
-        #             if self.edge_add(s1,s2):
-        #                 IncDegNode.remove(s1)
-        #                 IncDegNode.remove(s2)
-        #                 add_rmv += 1
-        #                 print( 'add edge %d : (%d,%d)'% (add_rmv,s1,s2))
-        #                 break
-        #             else:
-        #                 self.logger.warning( 'failed to add edge %d,%d'%(s1,s2))
-        #         if bound>50:
-        #             break
-        # i =0
-        # randomlist = random.sample(xrange(len(self.rho_plus)), len(self.rho_plus))
-        # if len(self.rho_minus) == len(self.rho_plus):
-        #     for index in randomlist:
-        #         self.edge_switch(self.rho_minus[index],self.rho_plus[i])
-        #         i+=1
-        #         #print('edge switch node1 = %d, node2 = %d' %(self.rho_minus[i],self.rho_plus[i]))
-        # else:
-        #     self.logger.warning('rho minus <> rho_plus')
 
         swarmPSO = SwarmBPSO (self.modified_graph, self.rho_plus, self.rho_minus, self.modified_omega_clusters)
         swarmPSO.initializeSwarm()
-
-
-
-
-
 
 
     def edge_removal (self, node1, node2):
@@ -180,16 +128,19 @@ class SwarmPDA():
             if bound > 50:
                 self.logger.warning('there is no pivot node for edge removal,try 50 times')
                 break
-        self.modified_graph.remove_edge(node1,neighbor1)
-        self.modified_graph.remove_edge(node2,neighbor2)
-        self.modified_graph.add_edge(neighbor1,neighbor2)
-        # edit modified omega cluster
-        for cluster in self.modified_omega_clusters:
-            for node in cluster:
-                if node['id'] in [node1,node2]:
-                    node['degree'] -=1
-                    node['rho'] -= 1
-        return True
+        if bound < 50:
+            self.modified_graph.remove_edge(node1,neighbor1)
+            self.modified_graph.remove_edge(node2,neighbor2)
+            self.modified_graph.add_edge(neighbor1,neighbor2)
+            # edit modified omega cluster
+            for cluster in self.modified_omega_clusters:
+                for node in cluster:
+                    if node['id'] in [node1,node2]:
+                        node['degree'] -=1
+                        node['rho'] -= 1
+            return True
+        else:
+            return False
 
     def edge_add (self, node1, node2):
         if node1 in self.modified_graph.neighbors(node2) or node1 == node2:
@@ -217,11 +168,13 @@ class SwarmBPSO:
     rho_plus = []
     dimension = 2
     nof_particle = 3
-    gBest = []
+    gBest = {'graph':None,'fitness': float('inf')}
     pBest = []
     logging.basicConfig()
     logger = logging.getLogger('SwarmBPSO')
     node_change = []
+    noi =10
+    original_f= 0
 
 
     def __init__(self,original_graph, rho_plus, rho_minus, original_omega_cluster):
@@ -229,6 +182,8 @@ class SwarmBPSO:
         self.omega_cluster = original_omega_cluster
         self.rho_minus = rho_minus
         self.rho_plus = rho_plus
+        print 'from init',len(self.rho_minus)
+        print 'from init',len(self.rho_plus)
 
 
     def initializeSwarm(self):
@@ -248,32 +203,52 @@ class SwarmBPSO:
         # calculate personal best for every particle
 
         print "calculate original FF ..."
-        original_fitness = self.fitness(self.original_g)
-        print "Original FF is:" , original_fitness
+        self.original_f = self.fitness(self.original_g)
+        print "Original FF is:" , self.original_f
+        iter = 0
+        self.gBest['fitness'] = self.original_f
+        print 'iter -> %i'%iter
         print 'Edge switch:'
         for i,dim in enumerate(self.modified_g):
             for j,g in enumerate(dim):
                 indx = ((i)*self.nof_particle)+(j+1)
+                for k,node in enumerate(self.swarm[i][j]):
+                    self.edge_switch(g,node,self.rho_minus[k])
                 if (self.dimension*self.nof_particle) == indx:
                     print '|','-' * (indx+1),'|', indx , '/' ,self.dimension*self.nof_particle
                 else:
                     print '|','-' * indx,' ' * ((self.dimension*self.nof_particle)- indx),'|',indx , '/' ,self.dimension*self.nof_particle
-                for k,node in enumerate(self.swarm[i][j]):
-                    self.edge_switch(g,node,self.rho_minus[k])
-        print 'personal best:'
+        self.Personal_Best()
+        self.Global_Best()
+
+    def Personal_Best (self):
+        print 'Calculate personal best'
         for i,dim in enumerate(self.modified_g):
             pb = []
             for j,g in enumerate(dim):
+                modified_fitness = self.fitness(g)
+                pb.append(self.original_f -  modified_fitness)
                 indx = ((i)*self.nof_particle)+(j+1)
                 if (self.dimension*self.nof_particle) == indx:
                     print '|','-' * (indx+1),'|', indx , '/' ,self.dimension*self.nof_particle
                 else:
                     print '|','-' * indx,' ' * ((self.dimension*self.nof_particle)- indx),'|',indx , '/' ,self.dimension*self.nof_particle
-                modified_fitness = self.fitness(g)
-                pb.append( abs(original_fitness -  modified_fitness))
 
             self.pBest.append(pb)
-        print self.pBest
+        print 'personal best :',self.pBest
+
+    def Global_Best(self):
+        print 'Calculate Global best'
+        for i,dim in enumerate(self.pBest):
+            for j,pbest in enumerate(dim):
+                indx = ((i)*self.nof_particle)+(j+1)
+                if (self.dimension*self.nof_particle) == indx:
+                    print '|','-' * (indx+1),'|', indx , '/' ,self.dimension*self.nof_particle
+                else:
+                    print '|','-' * indx,' ' * ((self.dimension*self.nof_particle)- indx),'|',indx , '/' ,self.dimension*self.nof_particle
+                if (self.gBest['fitness'] > abs(pbest)):
+                    self.gBest['fitness'],self.gBest['graph'] = pbest,self.modified_g[i][j]
+        print 'global best :',self.gBest
         # calculate global best
 
 
