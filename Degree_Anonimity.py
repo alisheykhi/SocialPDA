3e21eee92c25a87c82b2b6079d31f2572d600140
Ali Sheykhi <ali.sheykhi@me.com>
2017-09-02 16:28:26 +0430

    implement 4 measurements

--- Degree_Anonimity.py
+++ Degree_Anonimity.py
@@ -2,11 +2,11 @@ from graph_util import ReadGraph
 from micro_pda import MicroPDA
 from particle_pda1 import ParticlePDA
 from swarm_pda import SwarmPDA
-
+from Measurments import measurmnets
 
 #graph_blogs = ReadGraph("polblogs.gml")
 #graph_books = ReadGraph("polbooks.gml")
-graph_caida = ReadGraph("caida.txt")
+graph_caida = ReadGraph("caida.txt",'1_5')
 optimal_omega_cluster = MicroPDA(graph_caida.sorted_degree_sequence)
 particle_pda = ParticlePDA(omega_clusters=optimal_omega_cluster.omega_clusters,beta= 0.99,
                            removed_omega_clusters=optimal_omega_cluster.removed_omega_clusters)
@@ -14,5 +14,7 @@ particle_pda = ParticlePDA(omega_clusters=optimal_omega_cluster.omega_clusters,b
 anonymizedcluster = particle_pda.clusterWithAvg()
 swarm_pda = SwarmPDA(omega_clusters=particle_pda.clusters_avg_embedded,graph_G= graph_caida.G)
 sol = swarm_pda.run_swarm()
+measurment = measurmnets(graph_caida.G, sol['original'], sol['modified'])
+measure = measurment.calculate_measures()
 
 
--- Measurments.py
+++ Measurments.py
@@ -13,7 +13,25 @@ class measurmnets:
         self.sol_graph      = sol_graph
 
     def calculate_measures(self):
-        pass
+        print 'Harmonic measns of shortest path'
+        self.measure['h_o'] = self.harmonic_mean(self.original_graph)
+        self.measure['h_m'] = self.harmonic_mean(self.modified_graph)
+        self.measure['h_s'] = self.harmonic_mean(self.sol_graph)
+        print 'subgraph centrality'
+        # self.measure['s_o'] = self.subgraph_centrality(self.original_graph)
+        # self.measure['s_m'] = self.subgraph_centrality(self.modified_graph)
+        # self.measure['s_s'] = self.subgraph_centrality(self.sol_graph)
+        print 'modularity'
+        #self.measure['m_o'] = self.modularity(self.original_graph)
+        # self.measure['m_m'] = self.modularity(self.modified_graph)
+        # self.measure['m_s'] = self.modularity(self.sol_graph)
+        print 'transitivity'
+        self.measure['t_o'] = self.transitivity(self.original_graph)
+        # self.measure['t_m'] = self.transitivity(self.modified_graph)
+        # self.measure['t_s'] = self.transitivity(self.sol_graph)
+
+        return self.measure
+
     def harmonic_mean(self,graph):
         return harmonic_centrality(graph)
     def subgraph_centrality(self,graph):
@@ -22,3 +40,7 @@ class measurmnets:
         return nx.modularity_spectrum(graph)
     def transitivity(self,graph):
         return nx.transitivity(graph)
+    def max_eigenvalue_lambda(self,graph):
+        pass
+    def min_eigenvalue_mu(self,graph):
+        pass
